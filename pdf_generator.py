092d89031e79bce6e0f839da25681fe7a299bbf4
Matias Fanger <maattz@outlook.com>
2025-02-23 22:55:14 -0300

    Fix pdf generator duplicated

--- pdf_generator.py
+++ pdf_generator.py
@@ -7,31 +7,13 @@ def generate_pdf(json_data, output_file="presupuesto.pdf"):
         # Extraemos pdf_title y content del JSON
         pdf_title = json_data["pdf_title"]
         content = json_data["content"]
-        
-        # Construimos el HTML con el título dinámico
-        html = CSS_ESTILO + markdown(markdown_text)
-        weasyprint.HTML(string=html).write_pdf(output_file)
-        return output_file
-    except Exception as e:
-        raise Exception(f"Error al generar el PDF: {str(e)}")
-
-
-from markdown import markdown
-import weasyprint
-from config import CSS_ESTILO
-
-def generate_pdf(json_data, output_file="presupuesto.pdf"):
-    try:
-        # Extraemos pdf_title y content del JSON
-        pdf_title = json_data["pdf_title"]
-        content = json_data["content"]
 
         # Si no existe pdf_title, lo seteamos como "Presupuesto"
         if not pdf_title:
             pdf_title = "Presupuesto"
         
         # Construimos el HTML con el título dinámico
-        html = CSS_ESTILO + "Presupuesto" + markdown(content)  # Cambié markdown_text por content
+        html = CSS_ESTILO + "<h1>Presupuesto</h1>" + markdown(content)  # Cambié markdown_text por content
         weasyprint.HTML(string=html).write_pdf(pdf_title+'.pdf')
         return pdf_title+'.pdf'
     except Exception as e:
